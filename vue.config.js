8708037a4bfad18c75b87aeb024c70937cc53f89
lui714 <luiscuravalera20@gmail.com>
2022-03-16 19:09:53 +0100

    h

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,19 @@
 module.exports = {
-    chainWebpack: config => {
-      config.module
-        .rule('graphql')
-        .test(/.(graphql|gql)$/)
-        .use('graphql-tag/loader')
-        .loader('graphql-tag/loader')
-        .end();
-    }
-  };
+  chainWebpack: config => {
+    config.module
+      .rule('graphql')
+      .test(/.(graphql|gql)$/)
+      .use('graphql-tag/loader')
+      .loader('graphql-tag/loader')
+      .end();
+  }
+};
+
+module.exports = {
+  transpileDependencies: [
+    "@splidejs"
+  ]
+};
   
 const path = require('path')
 
